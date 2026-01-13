54687e3eca856ff682b9e28159883665296c035d
Lemi Melkamu <lemimelkamu@gmail.com>
2026-01-13 07:55:36 +0300

    Fix: fix environment variable issue on Docker file

--- lib/client.ts
+++ lib/client.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/v1',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || '/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
